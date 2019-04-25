7589536b3e6b2293a998dd18176906492d48f1d8
Edward Steere <edward@jemstep.com>
2019-04-25 13:50:11 +0200

    Fix the name of the jar in the prepare script

--- deployment/docker/prepare.sh
+++ deployment/docker/prepare.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cp "../../target/scala-2.12/todo http4s doobie-assembly-1.0-SNAPSHOT.jar" ./todo.jar
+cp "../../target/scala-2.12/todo-http4s-doobie-assembly-1.0-SNAPSHOT.jar" ./todo.jar
