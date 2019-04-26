596485316375d45790b1306c9a245309b0492625
Edward Steere <edward@jemstep.com>
2019-04-26 10:27:14 +0200

    Use a specific context and namespace for the deployment script

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -7,6 +7,22 @@ then
     echo "Usage arguments either up or down. eg: './deplpy.sh up'"
 fi
 
+if [ -z "$2" ]; then
+    echo "Plesae supply the second argument as the context to use for deployment."
+fi
+CONTEXT=$2
+
+if [ -z "$3" ]; then
+    echo "Plesae supply the third argument as namespace to use for deployment."
+fi
+NAMESPACE=$3
+
+echo "Switching to the context: $CONTEXT"
+kubectl config use-context "$CONTEXT"
+
+echo "Using the namespace: $NAMESPACE"
+kubectl config set-context --current --namespace="$NAMESPACE"
+
 if [[ $1 == "up" ]]
 then
     echo "Bringing todo-http4s-doobie up"
