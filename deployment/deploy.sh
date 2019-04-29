bbd92b8fe45ca7274cc552b9271aec7975a5bead
Edward Steere <edward@jemstep.com>
2019-04-29 16:17:27 +0200

    Setup minikube config

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -6,6 +6,10 @@ if [[ $1 != "up" ]] && [[ $1 != "down" ]]; then
     echo "Usage arguments either up or down. eg: './deplpy.sh up'"
 fi
 
+sed -i "s/MASTER_NODE_IP/$KUBERNETES_SERVICE_HOST/g" /home/go/config
+mkdir /home/go/.kube
+cp /home/go/config /home/go/.kube/config
+
 kubectl config set-cluster minikube --server="$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT"
 kubectl config use-context minikube
 
