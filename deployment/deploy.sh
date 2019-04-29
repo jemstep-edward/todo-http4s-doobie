644f24b6a6db5bd94c2f0b619dcc713fd7a31dcc
Edward Steere <edward@jemstep.com>
2019-04-29 15:55:00 +0200

    Attempt number 2M

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -2,75 +2,19 @@
 
 set -e
 
-if [[ $1 != "up" ]] && [[ $1 != "down" ]]
-then
+if [[ $1 != "up" ]] && [[ $1 != "down" ]]; then
     echo "Usage arguments either up or down. eg: './deplpy.sh up'"
 fi
 
-if [[ $1 == "up" ]]
-then
-    DEPLOYMENT_NAME=todo-http4s-doobie
+kubectl config set-cluster minikube --server="$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT" --api-version=v1
+kubectl config use-context minikube
 
-    if [ -z $KUBE_TOKEN ]; then
-        echo "FATAL: Environment Variable KUBE_TOKEN must be specified."
-        exit ${2:-1}
-    fi
-
-    if [ -z $NAMESPACE ]; then
-        echo "FATAL: Environment Variable NAMESPACE must be specified."
-        exit ${2:-1}
-    fi
-
-    echo
-    echo "Namespace $NAMESPACE"
-
-    status_code=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
-                       "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta2/namespaces/$NAMESPACE/deployments/$DEPLOYMENT_NAME" \
-                       -X GET -o /dev/null -w "%{http_code}")
-
-    if [ $status_code == 200 ]; then
-        echo
-        echo "Updating deployment"
-        curl --fail -H 'Content-Type: application/strategic-merge-patch+json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
-             "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta2/namespaces/$NAMESPACE/deployments/$DEPLOYMENT_NAME" \
-             -X PATCH -d @"$DEPLOYMENT_NAME.json"
-    else
-        echo
-        echo "Creating deployment"
-        curl --fail -H 'Content-Type: application/json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
-             "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta2/namespaces/$NAMESPACE/deployments" \
-             -X POST -d @"$DEPLOYMENT_NAME.json"
-    fi
-
-    status_code=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
-                       "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/$NAMESPACE/services/bulletin-board-service" \
-                       -X GET -o /dev/null -w "%{http_code}")
-
-    if [ $status_code == 404 ]; then
-        echo
-        echo "Creating service"
-        curl --fail -H 'Content-Type: application/json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
-             "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/$NAMESPACE/services" \
-             -X POST -d @bulletin-board-service.json
-    fi
-
-    status_code=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
-                       "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/extensions/v1beta1/namespaces/$NAMESPACE/ingresses/bulletin-board-ingress" \
-                       -X GET -o /dev/null -w "%{http_code}")
-
-    if [ $status_code == 404 ]; then
-        echo
-        echo "Creating ingress"
-        curl --fail -H 'Content-Type: application/json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
-             "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/extensions/v1beta1/namespaces/$NAMESPACE/ingresses" \
-             -X POST -d @bulletin-board-ingress.json
-    fi
+if [[ $1 == "up" ]]; then
     echo "Bringing todo-http4s-doobie up"
     kubectl apply -f todo-http4s-doobie.yml
 fi
 
-if [[ $1 == "down" ]]
-then
+if [[ $1 == "down" ]]; then
     echo "Bringing todo-http4s-doobie down"
     kubectl delete -f todo-http4s-doobie.yml
 fi
